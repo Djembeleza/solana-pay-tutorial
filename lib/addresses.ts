54465a6057300bc35adc53bb82d1728458f71421
Djembeleza <lkaola11@gmail.com>
2022-04-08 01:19:02 +0200

    Added my own var

--- lib/addresses.ts
+++ lib/addresses.ts
@@ -1,10 +1,9 @@
 import { PublicKey } from "@solana/web3.js"
 
 // Your shop wallet address
-export const shopAddress = new PublicKey('EXWr1Go8UyfA39U1dfRkcH8uUvqunbRhQEUQNm36UsyQ')
+export const shopAddress = new PublicKey('A35K56eta6LNuHjsZekSr7oFUZjkaNipGg3V73dd7d9d')
 
 // this is the same for everyone!
 export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
 
-// This is your token/coupon address
-export const couponAddress = new PublicKey('74aFyMgbKppdab4wEVz5AXL3UGbxMkwkwgK2LVzn3MmG')
+export const couponAddress = new PublicKey('8GyveckYF5XhvhRhSKg1ouxGJqB6oCQgtebH3FHLEtc6')
--- lib/products.ts
+++ lib/products.ts
@@ -1,18 +1,18 @@
 export const products = [
   {
-    id: 'box-of-cookies',
-    name: 'Box',
-    description: 'A delicious box of handmade cookies',
-    unitName: 'box', // shows after the price, eg. 0.05 SOL/box
-    priceSol: 0.05,
+    id: 'my5',
+    name: 'my5',
+    description: 'Enough for one joint. Recommended for the solo-stoners',
+    unitName: 'jay', // shows after the price, eg. 0.05 SOL/box
+    priceSol: 0.005,
     priceUsd: 5,
   },
   {
-    id: 'basket-of-cookies',
-    name: 'Basket',
-    description: 'A large basket of handmade cookies',
-    unitName: 'basket',
-    priceSol: 0.1,
+    id: 'my50',
+    name: 'X-Mas Banky',
+    description: 'Are you throwing a serious party? Then you probably need one or more.',
+    unitName: 'banky',
+    priceSol: 0.01,
     priceUsd: 10,
   }
 ]
